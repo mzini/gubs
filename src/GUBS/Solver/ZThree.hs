75c494ae5729b1dc5384960e55531c8c9e70643c
mschaper <michael.schaper@uibk.ac.at>
2017-04-02 13:26:26 +0200

    use `solverGetModel` in ZThree interface
    better performance with `solverGetModel`; unsafe variant that is used
    when SAT is already checked

--- src/GUBS/Solver/ZThree.hs
+++ src/GUBS/Solver/ZThree.hs
@@ -7,24 +7,28 @@ import qualified GUBS.MaxPolynomial           as MP
 import           GUBS.Solver.Class
 import qualified GUBS.Solver.Formula          as F
 
-import Data.Maybe (fromJust)
 import           Control.Monad.State.Strict
+import           Data.Maybe                   (fromJust)
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
 import qualified Z3.Monad                     as Z
 
 
+zthree :: SolverM ZThree a -> IO a
+zthree (Z3 z3) = Z.evalZ3With (Just Z.QF_NIA) Z.stdOpts z3
+
+
 data ZThree = ZThree deriving (Eq, Ord, Show)
 
 instance SMTSolver ZThree where
-  newtype SolverM ZThree a = Z3 { unZ3 :: Z.Z3 a } deriving (Functor, Applicative, Monad, MonadIO, Z.MonadZ3)
-  data NLiteral ZThree     = NLit Z.AST deriving (Eq, Ord, Show)
-  data BLiteral ZThree     = BLit Z.AST deriving (Eq, Ord, Show)
+  newtype SolverM ZThree a = Z3 { _runZ3 :: Z.Z3 a } deriving (Functor, Applicative, Monad, MonadIO, Z.MonadZ3)
+  newtype NLiteral ZThree  = NLit Z.AST              deriving (Eq, Ord)
+  newtype BLiteral ZThree  = BLit Z.AST
 
   freshBool = BLit <$> Z.mkFreshBoolVar "b"
   freshNat  = do
     n <- Z.mkFreshIntVar "n"
-    z <- Z.mkIntNum 0
+    z <- Z.mkInteger 0
     Z.assert =<< n `Z.mkGe` z
     return $ NLit n
 
@@ -34,11 +38,9 @@ instance SMTSolver ZThree where
   assertFormula     = aFormula
 
   checkSat          = (==) Z.Sat <$> Z.check
-  -- getValue (NLit n) = Z.getInt n -- MS: uses current context; not whole model
   getValue (NLit n) = do
-    (_,mM) <- Z.getModel
-    fromJust <$> Z.evalInt (fromJust mM) n
-
+    m <- Z.solverGetModel
+    fromJust <$> Z.evalInt m n
 
 
 aFormula :: SMTFormula ZThree -> SolverM ZThree ()
@@ -54,16 +56,11 @@ toFormula (Or a b)                 = Z.mkOr  =<< sequence [toFormula a, toFormul
 toFormula (And a b)                = Z.mkAnd =<< sequence [toFormula a, toFormula b]
 toFormula (Iff a b)                = join $ Z.mkIff <$> toFormula a <*> toFormula b
 
-
 toExpression :: SMTExpression ZThree -> SolverM ZThree Z.AST
 toExpression = fromMP . E.fromPolynomial MP.variable MP.constant where -- to exploit simplification
   fromMP (MP.Var (NLit v)) = return v
-  fromMP (MP.Const i)      = Z.mkIntNum i
+  fromMP (MP.Const i)      = Z.mkInteger i
   fromMP (MP.Plus a b)     = Z.mkAdd =<< sequence [fromMP a, fromMP b]
   fromMP (MP.Mult a b)     = Z.mkMul =<< sequence [fromMP a, fromMP b]
   fromMP MP.Max{}          = error "max encountered"
-
-zthree :: SolverM ZThree a -> IO a
--- zthree (Z3 z3) = Z.evalZ3With (Just Z.QF_NIA) Z.stdOpts z3
-zthree (Z3 z3) = Z.evalZ3 z3
 
