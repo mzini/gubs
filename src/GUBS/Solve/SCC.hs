d9bc7aa55a0e6dc6ca0b4ef1fac8eea7df7f481f
mschaper <michael.schaper@uibk.ac.at>
2017-05-10 20:32:39 +0200

    add variant of

--- src/GUBS/Solve/SCC.hs
+++ src/GUBS/Solve/SCC.hs
@@ -1,10 +1,11 @@
-module GUBS.Solve.SCC (sccDecompose, sccDecomposeWith) where
+module GUBS.Solve.SCC (sccDecompose, sccDecomposeWith, chainWith) where
 
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 
-import           GUBS.Solve.Strategy
 import           GUBS.Algebra
-import qualified GUBS.ConstraintSystem as CS
+import qualified GUBS.ConstraintSystem        as CS
+import           GUBS.Solve.Simplify          (partiallyInterpret)
+import           GUBS.Solve.Strategy
 
 
 sccDecompose :: (Eq f, Monad m) => Processor f c v m -> Processor f c v m
@@ -20,4 +21,16 @@ sccDecomposeWith f p cs =
   where
     toResult sccs (Progress []) = Progress (concat sccs)
     toResult _    _             = NoProgress
+
+-- @chainWith f p cs@ behaves similar to @try (exhaustive (sccDecomposeWith f p cs))@; but decomposition is applied
+-- only once.
+chainWith :: (Ord f, Ord v, Monad m, Integral c) => (CS.ConstraintSystem f v -> [[CS.TermConstraint f v]]) -> Processor f c v m -> Processor f c v m
+chainWith f p cs = go (f cs) where
+  go []     = return $ Progress []
+  go (s:ss) = do
+    logMsg ("Component: " ++ show (length ss + 1) ++ " Components")
+    q <- p s
+    case q of
+      Progress [] -> go ss
+      _           -> return $ Progress (concat $ s:ss)
 
